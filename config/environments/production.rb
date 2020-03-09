3b1f7408c144368951ce565cdcfecf6f4a9ae7b7
axaidan <axaidan@hotmail.com>
2020-03-09 11:59:24 +0100

    devise init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,9 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+	config.active_record.dump_schema_after_migration = false
+
+	# ============ DEVISE MAILER CONFIG ENV PROD =========
+	# config.action_mailer.default_url_options = { host => 'YOURAPPNAME.herokuapp.com' }
+
 end
