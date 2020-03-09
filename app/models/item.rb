6f832664459e984199343ca5b7d6c8350b17cbf7
Daniel <daniel.venera2@gmail.com>
2020-03-09 10:36:44 +0100

    10h36 item table and validations

---
+++ app/models/item.rb
@@ -0,0 +1,20 @@
+class Item < ApplicationRecord
+    validates :title, 
+    presence: true
+    validates_length_of :title,
+    minimum: 5
+
+
+    validates :description, 
+    presence: true
+    validates_length_of :description,
+    minimum: 10
+    
+    validates :price, 
+    presence: true,
+    numericality: {greater_than_or_equal_to: 5}
+
+    validates :image_url, 
+    presence: true 
+
+end
