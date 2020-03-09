8ae407eb47a0f4c1aefc4d626e0d1049d3f6fc91
AgathePlunian <agathe.plunian@gmail.com>
2020-03-09 14:00:59 +0100

    front bootswatch et navbar

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,8 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
 	# ========= CONFIG ACTION_MAILER ENV DEV ========== #
-	config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
