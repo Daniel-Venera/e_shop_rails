2cf74136e10fea6effdead6d188b1169a3863739
amine91480 <amine91480@hotmail.fr>
2020-03-12 16:48:26 +0100

    View history command -> OK

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,14 @@ class Order < ApplicationRecord
 	belongs_to :user
 	has_many :order_items, dependent: :destroy
 	has_many :items, through: :order_items
-	
+
+	def price_sum
+		sum = 0
+		self.items.each do |each_item|
+			sum += each_item.price
+		end
+		return sum
+	end
 
 	def command_send
 		UserMailer.command_email(self).deliver_now
