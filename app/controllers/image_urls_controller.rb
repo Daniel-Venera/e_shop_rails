cab041df296967187066be69d951a1eab85507cd
axaidan <axaidan@hotmail.com>
2020-03-12 11:47:16 +0100

    admin item management working, 2steps active storage edition in views/admin/items/index

--- app/controllers/image_urls_controller.rb
+++ app/controllers/image_urls_controller.rb
@@ -1,7 +1,7 @@
 class ImageUrlsController < ApplicationController
-    def create
-        @item = Item.find(params[:item_id])
-        @item.image_url.attach(params[:image_url])
-        redirect_to(item_path(@item))
-      end
+	def create
+		@item = Item.find(params[:item_id])
+		@item.image_url.attach(params[:image_url])
+		redirect_to(item_path(@item))
+	end
 end
